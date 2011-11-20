ceb7dc828bdfa85ba0a19aa6a4486149ac2fdffc
Marcos Quesada <marcos.quesadas@gmail.com>
2011-11-20 17:16:02 +0100

    sideBarMenu on Producto Admin

--- src/Base/TestBundle/Admin/ProductoAdmin.php
+++ src/Base/TestBundle/Admin/ProductoAdmin.php
@@ -7,6 +7,7 @@ use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Route\RouteCollection;
+use Knp\Menu\ItemInterface as MenuItemInterface;
 
 //use Application\Sonata\AdminBundle\Admin\Admin as Base;
 
@@ -161,5 +162,36 @@ class ProductoAdmin extends Admin
             ;
     }
 
+    protected function configureSideMenu(MenuItemInterface $menu, $action, Admin $childAdmin = null)
+    {
+        if (!$childAdmin && !in_array($action, array('edit'))) {
+            return;
+        }
+
+        $admin = $this->isChild() ? $this->getParent() : $this;
+
+        $id = $admin->getRequest()->get('id');
+
+        $menu->addChild(
+            $this->trans('view_post'),
+            array('uri' => $admin->generateUrl('show', array('id' => $id)))
+        );
+        
+        $menu->addChild(
+            $this->trans('test_1'),
+            array('uri' => $admin->generateUrl('edit', array('id' => $id)))
+        );        
+        
+        $menu->addChild(
+            $this->trans('createtype'),
+            array('uri' => $admin->generateUrl('createtype', array('id' => $id)))
+        );         
+        
+        $menu->addChild(
+            $this->trans('list'),
+            array('uri' => $admin->generateUrl('list'))
+        );         
+
+    }
 
 }
